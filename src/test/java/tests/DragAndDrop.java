a5b516ba578c5e4d12dc7b5733b0b4f751b2019a
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-04-16 15:35:45 +0900

    add

--- src/test/java/tests/DragAndDrop.java
+++ src/test/java/tests/DragAndDrop.java
@@ -1,22 +1,24 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class DragAndDrop extends TestBase {
+
     @Test
     void successfulFillTest() {
         step("Открываем главную страницу", (step) -> {
-        open("https://the-internet.herokuapp.com/drag_and_drop");
+            open("https://the-internet.herokuapp.com/drag_and_drop");
         });
         step("Drag and drop a to b", (step) -> {
-        $("#column-a").dragAndDropTo("#column-b");
+            $("#column-a").dragAndDropTo("#column-b");
         });
-            step("Проверка", (step) -> {
-        $("#column-a").shouldHave(text("B"));
-        $("#column-b").shouldHave(text("A"));
+        step("Проверка", (step) -> {
+            $("#column-a").shouldHave(text("B"));
+            $("#column-b").shouldHave(text("A"));
         });
     }
 }
